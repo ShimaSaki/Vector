b6d9e194dbf74ed86aa006c0d298cbbfb6ab5f3b
j1314 <j1314@PC4-40.cc.matsue-ct.jp>
2017-09-26 11:21:12 +0900

    addメソッドを追加

--- vector.rb
+++ vector.rb
@@ -1,4 +1,6 @@
 class Vector
+  attr_accessor :x, :y
+
   def initialize(x, y)
     @x = x
     @y = y
@@ -10,5 +12,11 @@ class Vector
 
   def length
     Math::sqrt(@x**2 + @y**2)
+  end
+
+  def add(v2)
+    x = @x+v2.x
+    y = @y+v2.y
+    Vector.new(x,y)
   end
 end
